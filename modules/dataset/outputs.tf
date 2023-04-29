c5c9ede355a4aaf5438f0ec707b0f4f1e3cebea1
semirfestic <119874378+semirfestic@users.noreply.github.com>
2023-04-29 09:52:49 +0200

    Delete modules/dataset directory

--- modules/dataset/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "dataset_ids" {
-  value = { for k, v in google_bigquery_dataset.datasets : k => v.dataset_id }
-}
--- modules/dataset/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" {
-  description = "The ID of the GCP project where the datasets and tables will be created."
-}
-
-variable "datasets" {
-  description = "A map of BigQuery datasets to create, where the key is the dataset name and the value is a map of dataset properties."
-  type        = map(any)
-}
