a6c46c098192ed540596d6d37143d4a18375781f
semirfestic <119874378+semirfestic@users.noreply.github.com>
2023-04-29 10:33:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "bigquery_datasets" {
   source = "./modules/finops-bigquery/datasets"
 
-  project_id = "vmware-migration-vmware-lab"
+  project_id = "finops-gcp-project"
   datasets = {
     "customer_x_cost_export_ds" = {
       friendly_name = "customer_x_cost_export_ds"
@@ -32,3 +32,38 @@ module "bigquery_datasets" {
     }
   }
 }
+module "bigquery_tables" {
+  source     = "./modules/finops-bigquery/tables"
+  project_id = "finops-gcp-project"
+
+  tables = {
+    "tech_summary_view" = {
+      dataset  = module.bigquery_datasets.dataset_ids["customer_x_cost_export_ds"]
+      location = "EU"
+      labels = {
+        dataset = "dashboard"
+      }
+      deletion_protection = false
+      query               = <<-EOF
+      SELECT
+        ts.execution_time,
+        ts.project_id,
+        pd.project_name,
+        ts.guest_vcpus,
+        ts.reserved_cores,
+        ts.gcs_storage_gb,
+        ts.pubsub_storage_kb,
+        ts.bq_storage_gb,
+        pd.product,
+        pd.domain
+      FROM
+        `var.monitoring_project.finops_data.technical_summary` AS ts
+      LEFT JOIN
+        `var.monitoring_project.finops_data.gcp_project_data_view` pd
+      ON
+        ts.project_id = pd.project_id
+    EOF
+    },
+  }
+}
+
