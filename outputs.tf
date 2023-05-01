5afae03e16ba70910fc90bd5eabd8222e753b198
semirfestic <119874378+semirfestic@users.noreply.github.com>
2023-05-01 20:58:48 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
+output "gcp_notification_channels" {
+  value = { for notification_id, channel_configs in module.finops_notification_channels :
+  notification_id => channel_configs }
+}
+
 output "dataset_ids" {
-  value = { for dataset_id, datasets in module.bigquery_datasets : 
+  value = { for dataset_id, datasets in module.bigquery_datasets :
   dataset_id => datasets }
 }
